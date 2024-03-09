98c2edb60558fd485e782ae67ef3bb391a568d58
Tarek <tarekfb69@gmail.com>
2024-03-09 23:13:16 +0100

    remove hr

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,12 +14,11 @@
 				{#if index > 0}
 					<a
 						href={`/${formatDate(post.data.created)}`}
-						class="inline-block bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-2xl"
+						class="inline-block bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-2xl mb-4"
 					>
 						Read full
 					</a>
 				{/if}
-				<hr class="w-full border-t-2 my-6 border-gray-300" />
 			</li>
 		{/each}
 	</ol>
